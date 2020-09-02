4ca9477ee45064aa58ea2dd76cb3180da587d901
Justin matte <crakdaple386@hotmail.com>
2020-09-02 15:47:33 -0400

    Uploaded files.

---
+++ StringBuilderDemo/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringBuilderDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StringBuilder sb = new StringBuilder("Hello");
+            sb.AppendLine("This is the second line. ");
+            sb.Append(".");
+
+            Console.WriteLine(sb.ToString());
+        }
+    }
+}
